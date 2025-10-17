0e3707550a75921c59f23d7005190f965fdf49bc
Vinh Nub <vinhnguyenvan1255@gmail.com>
2025-10-17 09:57:59 +0700

    cm

--- database/database_access.py
+++ database/database_access.py
@@ -1,15 +1,18 @@
 import sqlite3
+import os
 
 class Database:
     def __init__(self, path="database/data.db"):
+        os.makedirs(os.path.dirname(path), exist_ok=True)
         self.__conn = sqlite3.connect(path)
         self.__cursor = self.__conn.cursor()
+        self._execute_script("database/query.sql")
 
-    def get_conn(self):
-        return self.__conn
-    
-    def get_cursor(self):
-        return self.__cursor
-    
+    def _execute_script(self, script_path):
+        with open(script_path, "r", encoding="utf-8") as f:
+            sql_script = f.read()
+        self.__cursor.executescript(sql_script)
+        self.__conn.commit()
+
     def close(self):
         self.__conn.close()
